00f7fe8fb7ff9371631cd4d35400b87ece6ca97c
Bruno Macedo <brunom@ciandt.com>
2018-08-27 20:14:36 -0300

    first commit

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,7 @@
+import {  NgModule } from "@angular/core";
+import { PhotoComponent } from "./photo/photo.component";
+
+@NgModule({
+    declarations: [PhotoComponent]
+})
+export class PhotosModule {}
